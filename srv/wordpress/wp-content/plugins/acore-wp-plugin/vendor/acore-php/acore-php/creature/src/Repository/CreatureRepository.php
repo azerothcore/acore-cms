51f597ef9d940145e2ee66c1c301fc2b68bb7b02
Yehonal <yehonal.azeroth@gmail.com>
2020-06-06 18:09:23 +0200

    removed temporary the acore-php

--- srv/wordpress/wp-content/plugins/acore-wp-plugin/vendor/acore-php/acore-php/creature/src/Repository/CreatureRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ACore\Creature\Repository;
-
-use \ACore\System\Utils\Repository;
-
-class CreatureRepository extends Repository {
-    
-}
--- srv/wordpress/wp-content/plugins/acore-wp-plugin/vendor/acore-php/acore-php/defines/src/Paths.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ACore\Defines;
-
-class Paths {
-
-    const ROOT = __DIR__ . '../';
-
-}
