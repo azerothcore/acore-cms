86ccb6ab95b56f3a5c73758f57509e365fc3565f
Yehonal <yehonal.azeroth@gmail.com>
2020-06-06 18:02:52 +0200

    git subrepo pull srv/wordpress/wp-content/plugins/acore-wp-plugin/apps/git-utils
    subrepo:
      subdir:   "srv/wordpress/wp-content/plugins/acore-wp-plugin/apps/git-utils"
      merged:   "e01d64d"
    upstream:
      origin:   "https://github.com/azerothcore/git-utils"
      branch:   "master"
      commit:   "e01d64d"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

--- srv/wordpress/wp-content/plugins/acore-wp-plugin/apps/git-utils/subrepo.sh
+++ srv/wordpress/wp-content/plugins/acore-wp-plugin/apps/git-utils/subrepo.sh
@@ -16,10 +16,22 @@ function subrepoUpdate() {
     branch=$2
     folder=$3
 
-    # try-catch
-    set +e
-    git subrepo clone "$repo" "$folder" -b "$branch"
-    set -e
+    toClone=$(git ls-remote --heads "$repo" "$branch" | wc -l)
+
+    if [[ -d "$folder" ]]; then
+        if [[ ! -f "$folder/.gitrepo" ]]; then
+            git subrepo init "$folder" -r "$repo" -b "$branch"
+        fi
+
+        if [[ $toClone -eq 0 ]]; then
+            git subrepo push "$folder"
+        fi
+    else
+        # try-catch
+        set +e
+        git subrepo clone "$repo" "$folder" -b "$branch"
+        set -e
+    fi
 
     git subrepo clean "$folder"
     git subrepo pull "$folder"
