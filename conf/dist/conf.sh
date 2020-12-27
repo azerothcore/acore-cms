5d610b1f7eb18958b3552a80b76220af2a575027
Yehonal <yehonal.azeroth@gmail.com>
2020-12-27 16:33:35 +0000

    Reworked docker system

--- conf/dist/conf.sh
+++ conf/dist/conf.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+#########################
+#
+# MYSQL EXPORTER
+#
+#########################
+
+# Set the path where you want to store
+# exported sql dumps
 SQL_PATH="/data/sql/"
+
+
+# Mysql user credentials
 MYSQL_USER="wordpress"
 MYSQL_PASS="wordpress"
