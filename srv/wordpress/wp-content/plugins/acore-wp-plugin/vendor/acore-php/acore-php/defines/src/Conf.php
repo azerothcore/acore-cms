b33a82675bc5c5eb2cc46abfcaba642a06d3541a
Stefano Borzì <stefanoborzi32@gmail.com>
2021-09-22 11:31:59 +0200

    fix: reset and registration password generation @nicoaravena

--- srv/wordpress/wp-content/plugins/acore-wp-plugin/vendor/acore-php/acore-php/defines/src/Conf.php
+++ srv/wordpress/wp-content/plugins/acore-wp-plugin/vendor/acore-php/acore-php/defines/src/Conf.php
@@ -7,5 +7,5 @@ class Conf {
     const PASSWORD_LENGTH = 16;
     const USERNAME_LENGTH = 20;
     const PASSWORD_VALID_CHARS = "/[a-zA-Z0-9\!\"\#\$\%\&\'\(\)\*\+\,\\\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\}\~]/";
-    const PASSWORD_CHARS_LIST = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()+-.:;<=>?@[]{}";
+    const PASSWORD_CHARS_LIST = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 }
