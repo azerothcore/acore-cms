9cc15481fb6844eb8c30e4656fa2afb358e2d196
Nicolas Aravena <aravenam.nicolas@gmail.com>
2021-09-28 03:09:34 -0300

    Fix password validation

--- srv/wordpress/wp-content/plugins/acore-wp-plugin/src/services/UserService.php
+++ srv/wordpress/wp-content/plugins/acore-wp-plugin/src/services/UserService.php
@@ -6,30 +6,32 @@ use ACore\Defines\Conf;
 
 class UserService
 {
-    public static function validatePassword($Password)
+    public static function validatePassword($password)
     {
-        if (strlen($Password) > Conf::PASSWORD_LENGTH) {
+        if (strlen($password) > Conf::PASSWORD_LENGTH) {
             return sprintf(
                 __("Password is too long (%s), please use less then %s characters", 'acore_wp_plugin'),
-                strlen($Password), Conf::PASSWORD_LENGTH
+                strlen($password), Conf::PASSWORD_LENGTH
             );
         }
 
-        if (strlen($Password) < Conf::PASSWORD_MIN_LENGTH) {
+        if (strlen($password) < Conf::PASSWORD_MIN_LENGTH) {
             return sprintf(
                 __("Password is too short (%s), please use more then 4 characters", 'acore_wp_plugin'),
-                strlen($Password)
+                strlen($password)
             );
         }
 
-        if (!preg_match(Conf::PASSWORD_VALID_CHARS, $password )) {
-            $invalidChars = explode('', preg_replace(Conf::PASSWORD_VALID_CHARS, '', $password));
-            error_log(print_r($invalidChars));
+        $match = [];
+        preg_match( Conf::PASSWORD_VALID_CHARS, $password, $match );
+        if (!isset($match[0]) || empty($match[0]) || $match[0] !== $password) {
+            $invalidChars = str_split(preg_replace(Conf::PASSWORD_VALID_CHARS, '', $password));
             return sprintf(
-                __("Password has the following invalid characters: [%s]", 'acore_wp_plugin'),
-                implode(',', $invalidChars)
+                __("Password has the following invalid characters: <b>%s</b>", 'acore_wp_plugin'),
+                implode(' ', $invalidChars)
             );
         }
+        wp_die(__('asdf.'));
 
         //#### Password looks good
         return true;
