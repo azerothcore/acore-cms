be82a6c385e70f4f8831292ca33c0f8b03f0e1d9
Stefano Borzì <stefanoborzi32@gmail.com>
2022-03-20 21:07:10 +0100

    fix: prevent abuse via API of the item-restoration service (#102)
    Co-authored-by: Stefano Borzi <stefano.borzi@fedex.com>

--- srv/wordpress/wp-content/plugins/acore-wp-plugin/src/Components/Tools/ToolsApi.php
+++ srv/wordpress/wp-content/plugins/acore-wp-plugin/src/Components/Tools/ToolsApi.php
@@ -3,6 +3,7 @@
 namespace ACore\Components\Tools;
 
 use ACore\Manager\ACoreServices;
+use ACore\Manager\Opts;
 
 class ToolsApi {
     public static function ItemRestoreList($request) {
@@ -27,6 +28,12 @@ add_action( 'rest_api_init', function () {
    register_rest_route( ACORE_SLUG . '/v1', 'item-restore', array(
     'methods'  => 'POST',
     'callback' => function( $request ) {
+
+        // if free item-restoration is disabled, return
+        if (Opts::I()->acore_item_restoration != '1') {
+            return http_response_code(401);
+        }
+
         $data = $request->get_json_params();
         // return  var_dump($data);
         if (! $data['cname'] ) {
