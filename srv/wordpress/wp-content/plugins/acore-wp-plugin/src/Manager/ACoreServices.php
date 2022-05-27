c154c62aafc59c3569af71c3755634cec1e591de
Axel Cocat <ax.cocat@gmail.com>
2022-05-27 18:00:33 +0200

    feat: name unlock service

--- srv/wordpress/wp-content/plugins/acore-wp-plugin/src/Manager/ACoreServices.php
+++ srv/wordpress/wp-content/plugins/acore-wp-plugin/src/Manager/ACoreServices.php
@@ -165,6 +165,15 @@ class ACoreServices
 
     /**
      *
+     * @return ACore\Manager\Auth\Repository\AccountAccessRepository
+     */
+    public function getAccountAccessRepo()
+    {
+        return $this->emAuth->getRepository('ACore\Manager\Auth\Entity\AccountAccessEntity');
+    }
+
+    /**
+     *
      * @return ACore\Manager\Auth\Repository\AccountBannedRepository
      */
     public function getAccountBannedRepo()
