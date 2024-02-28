1e7632afcb5a0c000a4494e9a42f6dd644256b2b
Helias <stefanoborzi32@gmail.com>
2024-02-28 17:57:58 +0100

    feat(sync_subscription.php): implement synchronous subscription with pmpro membership plugin and acore_auth

--- srv/wordpress/wp-content/plugins/acore-wp-plugin/src/boot.php
+++ srv/wordpress/wp-content/plugins/acore-wp-plugin/src/boot.php
@@ -11,6 +11,8 @@ require_once ACORE_PATH_PLG . 'src/Components/ServerInfo/ServerInfo.php';
 require_once ACORE_PATH_PLG . 'src/Components/Tools/ToolsInfo.php';
 require_once ACORE_PATH_PLG . 'src/Components/UserPanel/UserMenu.php';
 
+require_once ACORE_PATH_PLG . 'src/Hooks/Subscriptions/sync_subscription.php';
+
 require_once ACORE_PATH_PLG . 'src/Hooks/Various/tgmplugin_activator.php';
 
 require_once ACORE_PATH_PLG . 'src/Hooks/User/Include.php';
