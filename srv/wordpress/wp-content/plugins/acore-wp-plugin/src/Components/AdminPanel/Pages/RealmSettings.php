be3e0d68d391c8b3bfd2c6331826656129665ace
alecsci <alex_cicu94@hotmail.com>
2024-09-18 00:25:48 +0300

    improve local setup
    - adjust readme
    - provide soap connectivity bash script
    - fix docker compose setup
    - provide defaults for SOAP/DB connection

--- srv/wordpress/wp-content/plugins/acore-wp-plugin/src/Components/AdminPanel/Pages/RealmSettings.php
+++ srv/wordpress/wp-content/plugins/acore-wp-plugin/src/Components/AdminPanel/Pages/RealmSettings.php
@@ -1,10 +1,16 @@
 <?php
-    use ACore\Manager\Opts;
+
+use ACore\Manager\Opts;
 ?>
 
 <div class="wrap">
     <h2><?= __('AzerothCore Settings', Opts::I()->page_alias) ?></h2>
     <p>Configure realm name and database connection.</p>
+    <div class="alert alert-warning" role="alert">
+        Each field includes a placeholder, which must be replaced before using the SOAP connection or other plugin features. 
+        Hit the Save changes button after you filled in all the fields.
+        The default values are the suggestions based on the default azerothcore docker setup.
+    </div>
     <form name="form-acore-settings" method="post" action="">
         <div class="card w-50 p-0">
             <div class="card-body">
@@ -19,7 +25,7 @@
                                 <label for="acore_realm_alias">Realm Alias:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_realm_alias" value="<?= Opts::I()->acore_realm_alias; ?>" size="20">
+                                <input type="text" name="acore_realm_alias" value="<?= Opts::I()->acore_realm_alias ?>" placeholder="AzerothCore" size="20">
                             </td>
                         </tr>
                     </tbody>
@@ -32,7 +38,7 @@
                                 <label for="acore_soap_host">Soap Host:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_soap_host" value="<?= Opts::I()->acore_soap_host; ?>" size="20">
+                                <input type="text" name="acore_soap_host" value="<?= Opts::I()->acore_soap_host ?>" placeholder="ac-worldserver" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -40,7 +46,7 @@
                                 <label for="acore_soap_port">Soap Port:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_soap_port" value="<?= Opts::I()->acore_soap_port; ?>" size="20">
+                                <input type="text" name="acore_soap_port" value="<?= Opts::I()->acore_soap_port ?>" placeholder="7878" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -48,7 +54,7 @@
                                 <label for="acore_soap_user">Soap User:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_soap_user" value="<?= Opts::I()->acore_soap_user; ?>" size="20" >
+                                <input type="text" name="acore_soap_user" value="<?= Opts::I()->acore_soap_user ?>" placeholder="admin" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -56,7 +62,7 @@
                                 <label for="acore_realm_alias">Soap Pass:</label>
                             </th>
                             <td>
-                                <input type="password" name="acore_soap_pass" value="<?= Opts::I()->acore_soap_pass; ?>" size="20" >
+                                <input type="password" name="acore_soap_pass" value="<?= Opts::I()->acore_soap_pass ?>" placeholder="admin" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -69,10 +75,10 @@
                     <tbody>
                         <tr>
                             <th scope="row">
-                            <label for="acore_db_auth_host">Database Auth Host:</label>
+                                <label for="acore_db_auth_host">Database Auth Host:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_auth_host" value="<?= Opts::I()->acore_db_auth_host; ?>" size="20">
+                                <input type="text" name="acore_db_auth_host" value="<?= Opts::I()->acore_db_auth_host; ?>" placeholder="ac-database" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -80,7 +86,7 @@
                                 <label for="acore_db_auth_port">Database Auth Port:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_auth_port" value="<?= Opts::I()->acore_db_auth_port; ?>" size="20">
+                                <input type="text" name="acore_db_auth_port" value="<?= Opts::I()->acore_db_auth_port; ?>" placeholder="3306" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -88,7 +94,7 @@
                                 <label for="acore_db_auth_user">Database Auth User:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_auth_user" value="<?= Opts::I()->acore_db_auth_user; ?>" size="20" >
+                                <input type="text" name="acore_db_auth_user" value="<?= Opts::I()->acore_db_auth_user; ?>" placeholder="root" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -96,7 +102,7 @@
                                 <label for="acore_realm_alias">Database Auth Pass:</label>
                             </th>
                             <td>
-                                <input type="password" name="acore_db_auth_pass" value="<?= Opts::I()->acore_db_auth_pass; ?>" size="20" >
+                                <input type="password" name="acore_db_auth_pass" value="<?= Opts::I()->acore_db_auth_pass; ?>" placeholder="password" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -104,7 +110,7 @@
                                 <label for="acore_db_auth_name">Database Auth Name:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_auth_name" value="<?= Opts::I()->acore_db_auth_name; ?>" size="20" >
+                                <input type="text" name="acore_db_auth_name" value="<?= Opts::I()->acore_db_auth_name; ?>" placeholder="acore_auth" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -120,7 +126,7 @@
                                 <label for="acore_db_char_host">Database Characters Host:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_char_host" value="<?= Opts::I()->acore_db_char_host; ?>" size="20">
+                                <input type="text" name="acore_db_char_host" value="<?= Opts::I()->acore_db_char_host; ?>" placeholder="ac-database" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -128,7 +134,7 @@
                                 <label for="acore_db_char_port">Database Characters Port:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_char_port" value="<?= Opts::I()->acore_db_char_port; ?>" size="20">
+                                <input type="text" name="acore_db_char_port" value="<?= Opts::I()->acore_db_char_port; ?>" placeholder="3306" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -136,7 +142,7 @@
                                 <label for="acore_db_char_user">Database Characters User:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_char_user" value="<?= Opts::I()->acore_db_char_user; ?>" size="20" >
+                                <input type="text" name="acore_db_char_user" value="<?= Opts::I()->acore_db_char_user; ?>" placeholder="root" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -144,7 +150,7 @@
                                 <label for="acore_realm_alias">Database Characters Pass:</label>
                             </th>
                             <td>
-                                <input type="password" name="acore_db_char_pass" value="<?= Opts::I()->acore_db_char_pass; ?>" size="20" >
+                                <input type="password" name="acore_db_char_pass" value="<?= Opts::I()->acore_db_char_pass; ?>" placeholder="password" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -152,23 +158,23 @@
                                 <label for="acore_db_char_name">Database Characters Name:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_char_name" value="<?= Opts::I()->acore_db_char_name; ?>" size="20" >
+                                <input type="text" name="acore_db_char_name" value="<?= Opts::I()->acore_db_char_name; ?>" placeholder="acore_characters" size="20">
                             </td>
                         </tr>
                         <tr>
                     </tbody>
                 </table>
 
-            <hr />
+                <hr />
 
                 <table class="form-table table table-borderless" role="presentation">
                     <tbody>
                         <tr>
                             <th scope="row">
-                            <label for="acore_db_world_host">Database World Host:</label>
+                                <label for="acore_db_world_host">Database World Host:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_world_host" value="<?= Opts::I()->acore_db_world_host; ?>" size="20">
+                                <input type="text" name="acore_db_world_host" value="<?= Opts::I()->acore_db_world_host; ?>" placeholder="ac-database" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -176,7 +182,7 @@
                                 <label for="acore_db_world_port">Database World Port:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_world_port" value="<?= Opts::I()->acore_db_world_port; ?>" size="20">
+                                <input type="text" name="acore_db_world_port" value="<?= Opts::I()->acore_db_world_port; ?>" placeholder="3306" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -184,7 +190,7 @@
                                 <label for="acore_db_world_user">Database World User:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_world_user" value="<?= Opts::I()->acore_db_world_user; ?>" size="20" >
+                                <input type="text" name="acore_db_world_user" value="<?= Opts::I()->acore_db_world_user; ?>" placeholder="root" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -192,7 +198,7 @@
                                 <label for="acore_realm_alias">Database World Pass:</label>
                             </th>
                             <td>
-                                <input type="password" name="acore_db_world_pass" value="<?= Opts::I()->acore_db_world_pass; ?>" size="20" >
+                                <input type="password" name="acore_db_world_pass" value="<?= Opts::I()->acore_db_world_pass; ?>" placeholder="password" size="20">
                             </td>
                         </tr>
                         <tr>
@@ -200,7 +206,7 @@
                                 <label for="acore_db_world_name">Database World Name:</label>
                             </th>
                             <td>
-                                <input type="text" name="acore_db_world_name" value="<?= Opts::I()->acore_db_world_name; ?>" size="20" >
+                                <input type="text" name="acore_db_world_name" value="<?= Opts::I()->acore_db_world_name; ?>" placeholder="acore_world" size="20">
                             </td>
                         </tr>
                         <tr>
